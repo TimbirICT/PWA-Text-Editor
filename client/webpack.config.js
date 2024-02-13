d5565ff6c020ca7cc5f6d197601c3056c588b5ab
Timbir <tmiddlebrooks99@gmail.com>
2024-02-12 21:41:53 -0600

    Revert "holy cow i made progress and things are happening"
    This reverts commit 80597f8012277af7b8064923024412dcc71085ba.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,54 +8,19 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        filename: 'index.html',
-      }),
-      new WebpackPwaManifest({
-        name: 'JATE',
-        short_name: 'JATE',
-        description: 'Text editor for browsers',
-        start_url: './',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#31a9e1',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-          },
-        ],
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
+      
     ],
+
     module: {
       rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
+        
       ],
     },
   };
